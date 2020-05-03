bf10aede29cdb870e0255f216a59b4bc11c1ff01
martin-linden <martin.linden@student.kyh.se>
2020-05-03 23:23:22 +0200

    gatsby gb-image moved to component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,46 +1,15 @@
 import React from 'react';
-/* import Image from '../components/image.js'; */
-import '../sass/main.scss';
-import Header from '../components/header';
-import SEO from '../components/seo';
-import BackgroundImage from 'gatsby-background-image';
-import { graphql } from 'gatsby';
-import transitionSVG from '../images/transition.svg';
+import Background from '../components/background';
 import ProjectPage from '../pages/projects';
 import Footer from '../components/footer';
+import '../sass/main.scss';
 
-const IndexPage = (props) => (
+const IndexPage = () => (
 	<div>
-		<BackgroundImage
-			fluid={props.data.indexImage.childImageSharp.fluid}
-			fadeIn
-		>
-			<Header />
-			<div className="masthead">
-				<div className="content-box">
-					<h1 className="h1">Welcome</h1>
-					<h2 className="h2">I'm Martin Lindén</h2>
-					<hr id="line" />
-					<h2 className="h2">Front-end Developer</h2>
-				</div>
-				<img id="transition" src={transitionSVG} />
-			</div>
-		</BackgroundImage>
+		<Background />
 		<ProjectPage />
 		<Footer />
 	</div>
 );
 
 export default IndexPage;
-
-export const pageQuery = graphql`
-	query {
-		indexImage: file(relativePath: { eq: "landscape.png" }) {
-			childImageSharp {
-				fluid(maxWidth: 1800) {
-					...GatsbyImageSharpFluid
-				}
-			}
-		}
-	}
-`;
