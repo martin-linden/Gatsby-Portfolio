ca19056814f8b760eaa04797eb0d6d0f304cf3cd
martin-linden <martin.linden@student.kyh.se>
2020-06-11 16:56:54 +0200

    added meta and title with react helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,28 @@ import Footer from '../components/footer';
 import '../sass/main.scss';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
+import Helmet from 'react-helmet';
 
 const IndexPage = () => (
-	<div>
-		<Background />
-		<ProjectPage />
-		<Footer />
-	</div>
+	<React.Fragment>
+		<Helmet>
+			<title>Martin Lindén - Front-End Developer</title>
+			<meta
+				name="description"
+				content="Front-end developer - HTML, CSS, React, JavaScript"
+			/>
+			<meta
+				property="og:title"
+				content="Martin Linden - Front-End Developer"
+			/>
+		</Helmet>
+
+		<div>
+			<Background />
+			<ProjectPage />
+			<Footer />
+		</div>
+	</React.Fragment>
 );
 
 export default IndexPage;
