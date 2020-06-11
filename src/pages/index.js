5bddceeed5c426cbd111cb7d73e52eddca87cd96
martin-linden <martin.linden@student.kyh.se>
2020-06-11 14:22:23 +0200

    new readme file

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import Background from '../components/background';
 import ProjectPage from '../pages/projects';
 import Footer from '../components/footer';
 import '../sass/main.scss';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 const IndexPage = () => (
 	<div>
