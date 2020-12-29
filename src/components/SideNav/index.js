a6c2e58149790df79e1526a69bd5d526fd7fcd04
martin-linden <martin.linden@student.kyh.se>
2020-12-29 13:06:38 +0100

    new sidenav design with icons

--- src/components/SideNav/index.js
+++ src/components/SideNav/index.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCode,
+  faEnvelope,
+  faHome,
+  faProjectDiagram,
+  faTasks,
+} from '@fortawesome/free-solid-svg-icons';
 
 export default function SideNav() {
   return (
     <ul className="sidenav-container">
       <li>
         <Link to="/" activeClassName="active-link">
-          <div className="button-container">
-            <button />
+          <div className="icon-wrapper">
+            <FontAwesomeIcon icon={faHome} color="#fff" size="xs" className="icon" />
           </div>
         </Link>
       </li>
       <li>
-        <div className="button-container">
-          <button />
+        <div className="icon-wrapper">
+          <FontAwesomeIcon icon={faCode} color="#fff" size="xs" className="icon" />
         </div>
       </li>
       <li>
         <Link to="/contact" activeClassName="active-link">
-          <div className="button-container">
-            <button />
+          <div className="icon-wrapper">
+            <FontAwesomeIcon icon={faEnvelope} color="#fff" size="xs" className="icon" />
           </div>
         </Link>
       </li>
